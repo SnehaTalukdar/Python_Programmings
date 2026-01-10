b5aa85564357f2ebab7fa0f0fe0768dc827a1762
Sneha Talukdar <SnehaTalukdar@users.noreply.github.com>
2026-01-11 00:25:25 +0530

    Python program to find the third largest number in an array

---
+++ code2.py
@@ -0,0 +1,21 @@
+#Python program to find the third largest number in an array 
+
+def findThirdlargest(arr):
+    n = len(arr)
+    arr.sort() #ascending order
+
+    #checking for third largest number
+    for i in range(n-3, -1, -1):
+
+        #Checking so that the number is not the first largest one
+        if arr[i] != arr[n-1]:
+            return arr[i]
+    
+#Main Block
+if __name__ == "__main__":
+    n = int(input("Total Numbers In The Array: "))
+arr = list(map(int, input("Enter The Array: ").split()))
+print (findThirdlargest(arr))
+
+
+    
